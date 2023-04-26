d149ec784c3dd06631be334bd452e625fdbade34
fer1su <rafael.rodrigues022@hotmail.com>
2023-04-26 18:54:35 -0300

    operadores

---
+++ index.js
@@ -0,0 +1,52 @@
+// +, -, *, /, %
+//const variavel1 = 10
+//const variavel2 = 20
+//const resultado = variavel1 / variavel2
+//console.log(resultado)
+
+//let idade = Number(prompt("Digite sua idade"))
+//idade = idade + 10
+//console.log(idade)
+
+///////////////////////////////////////////////////////////
+
+//const soma1 = 3
+//const soma2 = 4
+//const resultado1 = soma1 + soma2
+//console.log(resultado1)
+
+//const soma3 = 5
+//const soma4 = 2
+//const resultado2 = soma1 * soma3
+//const resultado3 = resultado2 / soma4
+//console.log(resultado3)
+
+//const numero = -1
+//const resultado4 = soma3 - soma2
+//const resultado5 = resultado4 * numero
+//console.log(resultado5)
+
+//const numero2 = 234
+//const resultado6 = numero2 % soma3
+//console.log(resultado6)
+
+////////////////////////////////////////////////////////////
+
+// >, <, ===
+//console.log(1 === 2)
+//const numero1 = 10
+//const numero2 = 20
+//console.log (numero1 === numero2)
+//console.log (numero1 !== numero2)
+//console.log (numero1 > numero2)
+//console.log (numero1 >= numero2)
+//console.log (numero1 < numero2)
+//console.log (numero1 <= numero2)
+
+// const nome = prompt("Digite o seu nome")
+// const idade = Number(prompt ("Digite sua idade"))
+// const idade2 = 18
+// const idade2050 = 27
+// console.log(nome, idade, idade2 <= idade, idade + idade2050 )
+
+
